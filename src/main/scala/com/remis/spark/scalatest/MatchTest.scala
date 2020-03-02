c856ed9313feca42939195b8540940cc05fdb890
remis <remisharoon@gmail.com>
2020-03-02 15:21:38 +0400

    testing scala futures

---
+++ src/main/scala/com/remis/spark/scalatest/MatchTest.scala
@@ -0,0 +1,16 @@
+package com.remis.spark.scalatest
+
+object MatchTest {
+
+  def main(args: Array[String]): Unit = {
+
+    5 match {
+
+      case 5 => println("5")
+      case 1 => println("1")
+      case none => println("none")
+      println("default")
+    }
+
+  }
+}
