dc2849e998bcbb43ee49cf74f84f8e9450fad455
remis <remisharoon@gmail.com>
2020-04-16 18:19:06 +0400

    HashJoin test

---
+++ src/main/scala/com/remis/spark/scalatest/concurrency/ObjectMultiThreadingTest.scala
@@ -0,0 +1,38 @@
+package com.remis.spark.scalatest.concurrency
+import scala.util.Random
+
+object ObjectMultiThreadingTest {
+
+  def main(args: Array[String]): Unit = {
+
+    for (x <- 1 to 5) {
+      var thread = new MyThread()
+      thread.setName("Thread number :" + x.toString)
+      thread.start()
+    }
+  }
+
+  class MyThread extends Thread {
+    override def run(): Unit = {
+      Singleton.myname = this.getName
+      Singleton.call(this.getName)
+    }
+  }
+
+  object Singleton {
+    var myname = "Singleton"
+    def call(threadName: String): Unit = {
+      val thName = threadName
+      val random = new Random
+      for (i <- 1 to 5) {
+        println("My Name is set by " + myname)
+        val sleeptime = random.nextInt(1000)
+        println(
+          "But, I am now running " + thName + ", sleeping for " + sleeptime
+        )
+        Thread.sleep(sleeptime)
+      }
+    }
+
+  }
+}
